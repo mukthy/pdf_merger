f07f483adeebda24431ab01ea4a30513d34ea952
Mukthy <muktheeswaran.m@gmail.com>
2022-04-02 07:54:11 +0530

    Updated Watermark PDF

--- watermark_pdf.py
+++ watermark_pdf.py
@@ -1,17 +1,21 @@
 import PyPDF2
+import sys
 
-files = PyPDF2.PdfFileReader(open("twopage.pdf", "rb"))
-watermark = PyPDF2.PdfFileReader(open("wtr.pdf", "rb"))
+files = sys.argv[1]
+watermark = sys.argv[2]
+
+pdf_files = PyPDF2.PdfFileReader(open(f"{files}", "rb"))
+watermark_file = PyPDF2.PdfFileReader(open(f"{watermark}", "rb"))
 new_pdf = PyPDF2.PdfFileWriter()
 
 
-pages = int(files.getNumPages())
+pages = int(pdf_files.getNumPages())
 
 
 for i in range(pages):
-    page = files.getPage(i)
+    page = pdf_files.getPage(i)
     print(i)
-    watermark_page = watermark.getPage(0)
+    watermark_page = watermark_file.getPage(0)
     page.mergePage(watermark_page)
     new_pdf.addPage(page)
     new_pdf.write(open('watermarked.pdf', 'wb'))
